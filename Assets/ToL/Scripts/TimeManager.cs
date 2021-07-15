f4be7f95c1668d8acaf9d5cfdde1ef89c47474df
Jordan Hanna <1015971@student.sae.edu.au>
2021-07-15 10:28:11 +1000

    Time Manager
    Starting working on Time Manager , will work on using a custom script

---
+++ Assets/ToL/Scripts/TimeManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public float slowdownFactor = 0.5f;
+    public float slowdownLength = 2f;
+    public TimeManager timeManager;
+
+    public void DoSlowMotion()
+    {
+        Time.timeScale = slowdownFactor;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("SLOW TIME");
+            timeManager.DoSlowMotion();
+        }
+    }
+}
