572d39ecb02d6b676041aa37430147b122caad0a
Vincent <3d314831994@gmail.com>
2021-07-19 14:36:23 +1000

    Revised Spawn Manager

--- Assets/ToL/Scripts/SpawnManager.cs
+++ Assets/ToL/Scripts/SpawnManager.cs
@@ -4,19 +4,12 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject TargetObject;
     public GameObject Object;
     public Vector2 xPos;
     public Vector2 zPos;
     public int ObjectCount;
     public GameObject [] arrayList;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
     public void SpawnObject()
     {
         for (int i = 0; i < arrayList.Length; i++)
@@ -24,23 +17,11 @@ public class SpawnManager : MonoBehaviour
             Instantiate(Object, arrayList[i].transform.position, Quaternion.Euler(0, 180, 0));
         }
     }
-
-    bool CheckSpawn(int count)
-    {
-        if (count > 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    IEnumerator spawnObject()
-    {
-        Vector3 SpawnPos = new Vector3(Random.Range(xPos.x, xPos.y), 0, Random.Range(zPos.x, zPos.y));
-        ObjectCount -= 1;
-        yield return new WaitForSeconds(1f);
-    }
+  
+    //IEnumerator spawnObject()
+    //{
+    //    Vector3 SpawnPos = new Vector3(Random.Range(xPos.x, xPos.y), 0, Random.Range(zPos.x, zPos.y));
+    //    ObjectCount -= 1;
+    //    yield return new WaitForSeconds(1f);
+    //}
 }
---
+++ Assets/ToL/Scripts/SunFlowerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunFlowerController : MonoBehaviour
+{
+    Animator anim;
+    public Vector2 growingTimeRange;
+
+    private void OnEnable()
+    {
+        anim = GetComponent<Animator>();
+        StartCoroutine(StartGrowing());
+    }
+
+    IEnumerator StartGrowing() {
+        yield return new WaitForSeconds(GetRandomTime());
+        anim.SetFloat("Speed",1);
+    }
+
+    private float GetRandomTime() {
+        return Random.Range(growingTimeRange.x,growingTimeRange.y);
+    }
+}
