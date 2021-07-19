4cd816dda1412fa33685f4f1a85d66ffd8745da7
Jordan Hanna <1015971@student.sae.edu.au>
2021-07-19 15:43:54 +1000

    Skyboxes
    Implemented Skyboxes for testing

---
+++ Assets/ToL/Scripts/WaterNoise.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterNoise : MonoBehaviour
+{
+
+    public float power = 0.2f;
+    public float scale = 1;
+    public float timeScale = 0.5f;
+
+    private float offsetX;
+    private float offsetY;
+    private MeshFilter filter;
+
+
+    void Start()
+    {
+        filter = GetComponent<MeshFilter>();
+        MakeNoise();
+    }
+
+    void Update()
+    {
+        MakeNoise();
+        offsetX += Time.deltaTime * timeScale;
+        offsetY += Time.deltaTime * timeScale;
+    }
+
+    void MakeNoise()
+    {
+        Vector3[] vertices = filter.mesh.vertices;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i].y = CalculateHeight(vertices[i].x, vertices[i].z) * power;
+        }
+
+        filter.mesh.vertices = vertices;
+    }
+
+    float CalculateHeight(float x, float y)
+    {
+        float xCord = x * scale + offsetX;
+        float yCord = y * scale + offsetY;
+
+        return Mathf.PerlinNoise(xCord, yCord);
+    }
+}
