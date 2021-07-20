16fbbd3c3907baf7dda9c1e89671f9a1fcbdb1cc
Vincent <3d314831994@gmail.com>
2021-07-20 16:24:43 +1000

    Added Fading text

---
+++ Assets/ToL/Scripts/Opening.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opening : MonoBehaviour
+{
+    public List<GameObject> textOrder;
+    public float timeBetween = 2f;
+    private int orderIndex;
+
+    private void Update()
+    {
+        if (orderIndex < textOrder.Count - 1)
+        {
+            if (!textOrder[orderIndex].gameObject.activeInHierarchy)
+            {
+                orderIndex++;
+                textOrder[orderIndex].SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/ToL/Scripts/TextFading.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TextFading : MonoBehaviour
+{
+    Animator anim;
+    public float preDisplayTime = 2f;
+    public float displayTime = 3f;
+    public float fadeOutTime = 1f;
+    public UnityEvent OnCompleted;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(PreDisplay());
+    }
+
+    IEnumerator PreDisplay() {
+        yield return new WaitForSeconds(preDisplayTime);
+        anim.Play("FadeIn");
+        StartCoroutine(FadeOut());
+    }
+
+    IEnumerator FadeOut() {
+        yield return new WaitForSeconds(displayTime);
+        anim.Play("FadeOut");
+        yield return new WaitForSeconds(fadeOutTime);
+        OnCompleted?.Invoke();
+        gameObject.SetActive(false);
+    }
+
+
+   
+}
