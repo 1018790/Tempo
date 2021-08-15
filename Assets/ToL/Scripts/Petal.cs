ed83534c2b2763c9aa747a8c16b3c109ab657b3f
Vincent <3d314831994@gmail.com>
2021-08-15 17:18:55 +1000

    Petals fading

---
+++ Assets/ToL/Scripts/Petal.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System;
+
+public class Petal : MonoBehaviour
+{
+    [SerializeField]
+    private SunflowerPetalsFading petalMaster;
+    public float fadeInTime = 0.19f;
+    public float fadeOutTime = 0.28f;
+    Image thisImage;
+    private void Awake()
+    {
+        thisImage = GetComponent<Image>();
+    }
+
+    public void FadeInFunction() {
+        StartCoroutine(FadeIn());
+    }
+
+    public void FadeOutFunction() {
+        StartCoroutine(FadeOut());
+    }
+
+    IEnumerator FadeIn() { 
+        thisImage.CrossFadeAlpha(1,fadeInTime,false);
+        yield return new WaitForSeconds(fadeInTime);
+        petalMaster.AddIndex();
+        //OnFadeInCompleted?.Invoke();
+    }
+
+    IEnumerator FadeOut() {
+        thisImage.CrossFadeAlpha(0, fadeOutTime, false);
+        yield return new WaitForSeconds(fadeOutTime);
+        petalMaster.DecreaseIndex();
+    }
+
+}
---
+++ Assets/ToL/Scripts/SunflowerPetalsFading.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class SunflowerPetalsFading : MonoBehaviour
+{
+    public TimeManager timeManager;
+    [SerializeField]
+    private Transform petalParent;
+    [SerializeField]
+    private GameObject[] petals;
+    private int petalIndex;
+    private bool isFading;
+
+    void Start()
+    {
+        petals = new GameObject[petalParent.childCount];
+
+        for (int i = 0; i < petals.Length; i++)
+        {
+            petals[i] = petalParent.GetChild(i).gameObject;
+            petals[i].GetComponent<Image>().CrossFadeAlpha(0,0.1f,false);
+        }
+        Array.Reverse(petals);
+    }
+
+    public void AddIndex() {
+        if (petalIndex < petals.Length-1) {
+            petalIndex++;
+            petals[petalIndex].GetComponent<Petal>().FadeInFunction();
+        }
+    }
+
+    public void DecreaseIndex() {
+        if (petalIndex > 0)
+        {
+            petalIndex--;
+            petals[petalIndex].GetComponent<Petal>().FadeOutFunction();
+        }
+    }
+
+    private void Update()
+    {
+        if (timeManager.canBreathe)
+        {
+            switch (timeManager.state)
+            {
+                case BreathingState.Inhale:
+                    if (!isFading)
+                    {
+                        petals[petalIndex].GetComponent<Petal>().FadeInFunction();
+                        isFading = true;
+                    }
+
+                    break;
+                case BreathingState.Exhale:
+                    if (isFading)
+                    {
+                        petals[petalIndex].GetComponent<Petal>().FadeOutFunction();
+                        isFading = false;
+                    }
+                    break;
+            }
+        }
+
+    }
+
+}
